51a4d3b9db590e827f16d1e216cde822d63e60ad
George Jimy <knivespeans@gmail.com>
2025-04-03 13:13:33 -0500

    seteo

--- Assets/Scripts/ImageUi.cs
+++ Assets/Scripts/ImageUi.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class ImageUi : MonoBehaviour
 {
+    public static ImageUi instance;
+    public GameObject imageWorldParent;
+    public GameObject imageUiParent;
     public List<Images> images = new List<Images>();
 
     private void Start()
     {
-        foreach (Images image in images)
+        if (imageWorldParent.transform.childCount != imageUiParent.transform.childCount)
         {
-            image.name = image.imageUi.name;
+            Debug.LogError("Los objetos hijos de imageWorldParent e imageUiParent no coinciden en cantidad.");
+            return;
         }
+        images.Clear();
+        for (int i = 0; i < imageWorldParent.transform.childCount; i++)
+        {
+            GameObject worldChild = imageWorldParent.transform.GetChild(i).gameObject;
+            GameObject uiChild = imageUiParent.transform.GetChild(i).gameObject;
+
+            Images newImage = new Images
+            {
+                name = uiChild.name,
+                imageWorld = worldChild,
+                imageUi = uiChild
+            };
+
+            // Añade la instancia a la lista
+            images.Add(newImage);
+        }
+        instance = this;
     }
     public void OpenWordUi(string name)
     {
